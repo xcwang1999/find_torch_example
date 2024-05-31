30bfcd68d91269aa343ec64dbe493f1e9ea6ee34
Xincheng Wang <xcwang.post@gmail.com>
2024-05-31 20:48:10 +0800

    update

--- bindings.cpp
+++ bindings.cpp
@@ -1,12 +1,7 @@
 #include <pybind11/pybind11.h>
-#include <torch/extension.h>
 
-torch::Tensor add_tensors(const torch::Tensor& a, 
-                          const torch::Tensor& b);
-
-namespace py = pybind11;
+void build_module(pybind11::module &m); 
 
 PYBIND11_MODULE(torch_cmake_example, m) {
-  m.def("add_tensors", &add_tensors, "Add two tensors");
+  build_module(m);
 }
-
--- torch_add.cpp
+++ torch_add.cpp
@@ -4,3 +4,7 @@ torch::Tensor add_tensors(const torch::Tensor& a,
                           const torch::Tensor& b) {
   return a + b;
 }
+
+void build_module(pybind11::module &m) {
+  m.def("add_tensors", &add_tensors, "Add two tensors");
+}
